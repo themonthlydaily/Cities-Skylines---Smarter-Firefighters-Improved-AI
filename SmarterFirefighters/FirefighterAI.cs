e05149c5482193857528326e8d6036d7d9b7ad68
Tal <talrofeh@gmail.com>
2022-01-09 13:33:56 +0200

    fix remove vehicles bug made by calling aad guest vehicle number of times (actual method and postfix)

--- SmarterFirefighters/FirefighterAI.cs
+++ SmarterFirefighters/FirefighterAI.cs
@@ -3,6 +3,7 @@ using System;
 using ColossalFramework;
 using ColossalFramework.Math;
 using UnityEngine;
+using System.Reflection;
 
 namespace SmarterFirefighters
 {
@@ -95,69 +96,246 @@ namespace SmarterFirefighters
         }
     }
 
-    // Instruct firetrucks to respond to fires that they drive by when returning to the station or nearby fires when waiting for a new target
-    // example from https://github.com/pardeike/Harmony/issues/132
-    // [HarmonyPatch(typeof(PedestrianPathAI), nameof(PedestrianPathAI.SimulationStep), new Type[] { typeof(ushort), typeof(NetSegment) }, new ArgumentType[] { ArgumentType.Normal, ArgumentType.Ref })]
-    [HarmonyPatch(typeof(FireTruckAI), "SimulationStep", new Type[] { typeof(ushort), typeof(Vehicle), typeof(Vehicle.Frame), typeof(ushort), typeof(Vehicle), typeof(int) }, new ArgumentType[] { ArgumentType.Normal, ArgumentType.Ref, ArgumentType.Ref, ArgumentType.Normal, ArgumentType.Ref, ArgumentType.Normal })]
+    [HarmonyPatch(typeof(FireTruckAI))]
     public static class FireTruckAISimulationStepPatch
     {
-        public static void Postfix(ushort vehicleID, ref Vehicle vehicleData)
+        private delegate void CarAISimulationStepDelegate(CarAI instance, ushort vehicleID, ref Vehicle vehicleData, ref Vehicle.Frame frameData, ushort leaderID, ref Vehicle leaderData, int lodPhysics);
+        private static readonly CarAISimulationStepDelegate BaseSimulationStep = AccessTools.MethodDelegate<CarAISimulationStepDelegate>(typeof(CarAI).GetMethod("SimulationStep", BindingFlags.Instance | BindingFlags.Public, null, new Type[] { typeof(ushort), typeof(Vehicle).MakeByRefType(), typeof(Vehicle.Frame).MakeByRefType(), typeof(ushort), typeof(Vehicle).MakeByRefType(), typeof(int) }, new ParameterModifier[] { }), null, false);
+
+        private delegate bool ArriveAtTargetDelegate(FireTruckAI instance, ushort vehicleID, ref Vehicle data);
+        private static ArriveAtTargetDelegate ArriveAtTarget = AccessTools.MethodDelegate<ArriveAtTargetDelegate>(typeof(FireTruckAI).GetMethod("ArriveAtTarget", BindingFlags.Instance | BindingFlags.NonPublic), null, false);
+
+        private delegate bool ExtinguishFireDelegate(FireTruckAI instance, ushort vehicleID, ref Vehicle data, ushort buildingID, ref Building buildingData);
+        private static ExtinguishFireDelegate ExtinguishFire = AccessTools.MethodDelegate<ExtinguishFireDelegate>(typeof(FireTruckAI).GetMethod("ExtinguishFire", BindingFlags.Instance | BindingFlags.NonPublic), null, false);
+
+        private delegate void SetTargetDelegate(FireTruckAI instance, ushort vehicleID, ref Vehicle data, ushort targetBuilding);
+        private static SetTargetDelegate SetTarget = AccessTools.MethodDelegate<SetTargetDelegate>(typeof(FireTruckAI).GetMethod("SetTarget", BindingFlags.Instance | BindingFlags.Public), null, false);
+
+        private delegate bool CanLeaveDelegate(FireTruckAI instance, ushort vehicleID, ref Vehicle vehicleData);
+        private static CanLeaveDelegate CanLeave = AccessTools.MethodDelegate<CanLeaveDelegate>(typeof(FireTruckAI).GetMethod("CanLeave", BindingFlags.Instance | BindingFlags.Public), null, false);
+
+        private delegate bool ShouldReturnToSourceDelegate(FireTruckAI instance, ushort vehicleID, ref Vehicle data);
+        private static ShouldReturnToSourceDelegate ShouldReturnToSource = AccessTools.MethodDelegate<ShouldReturnToSourceDelegate>(typeof(FireTruckAI).GetMethod("ShouldReturnToSource", BindingFlags.Instance | BindingFlags.NonPublic), null, false);
+
+        [HarmonyPatch(typeof(FireTruckAI), "SimulationStep", 
+            new Type[] { typeof(ushort), typeof(Vehicle), typeof(Vehicle.Frame), typeof(ushort), typeof(Vehicle), typeof(int) }, 
+            new ArgumentType[] { ArgumentType.Normal, ArgumentType.Ref, ArgumentType.Ref, ArgumentType.Normal, ArgumentType.Ref, ArgumentType.Normal })]
+        [HarmonyPrefix]
+
+        public static bool SimulationStep(FireTruckAI __instance, ushort vehicleID, ref Vehicle vehicleData, ref Vehicle.Frame frameData, ushort leaderID, ref Vehicle leaderData, int lodPhysics)
         {
-            if ((vehicleData.m_flags & (Vehicle.Flags.GoingBack | Vehicle.Flags.WaitingTarget)) != 0)
-            {
-                ushort newTarget = NewFireAI.FindBurningBuilding(vehicleData.GetLastFramePosition(), 40f);
-                if (newTarget != 0)
+            frameData.m_blinkState = (((vehicleData.m_flags & (Vehicle.Flags.Emergency1 | Vehicle.Flags.Emergency2)) == 0) ? 0f : 10f);
+		    BaseSimulationStep(__instance, vehicleID, ref vehicleData, ref frameData, leaderID, ref leaderData, lodPhysics);
+		    bool flag = false;
+            ushort newTarget = 0;
+		    if (vehicleData.m_targetBuilding != 0)
+		    {
+			    BuildingManager instance = Singleton<BuildingManager>.instance;
+			    Vector3 vector = instance.m_buildings.m_buffer[vehicleData.m_targetBuilding].CalculateSidewalkPosition();
+			    flag = (vector - frameData.m_position).sqrMagnitude < 4096f;
+			    bool flag2 = (vehicleData.m_flags & Vehicle.Flags.Stopped) != 0 || frameData.m_velocity.sqrMagnitude < 0.0100000007f;
+			    if (flag && (vehicleData.m_flags & Vehicle.Flags.Emergency2) != 0)
+			    {
+				    vehicleData.m_flags = (vehicleData.m_flags & ~Vehicle.Flags.Emergency2) | Vehicle.Flags.Emergency1;
+			    }
+			    if (flag && flag2)
+			    {
+				    if (vehicleData.m_blockCounter > 8)
+				    {
+					    vehicleData.m_blockCounter = 8;
+				    }
+				    if (vehicleData.m_blockCounter == 8 && (vehicleData.m_flags & Vehicle.Flags.Stopped) == 0)
+				    {
+					    ArriveAtTarget(__instance, leaderID, ref leaderData);
+				    }
+                    // if finish extinguish search for new 
+				    if (ExtinguishFire(__instance, vehicleID, ref vehicleData, vehicleData.m_targetBuilding, ref Singleton<BuildingManager>.instance.m_buildings.m_buffer[vehicleData.m_targetBuilding]))
+				    {
+                        newTarget = NewFireAI.FindBurningBuilding(vehicleData.GetLastFramePosition(), 40f);
+                        if(newTarget != 0)
+                        {
+                            SetTarget(__instance, vehicleID, ref vehicleData, newTarget);
+                        }
+                        else
+                        {
+                            SetTarget(__instance, vehicleID, ref vehicleData, 0);
+                        }
+				    }
+			    }
+			    else if (instance.m_buildings.m_buffer[vehicleData.m_targetBuilding].m_fireIntensity == 0)
+			    {
+				    SetTarget(__instance, vehicleID, ref vehicleData, 0);
+			    }
+		    }
+		    if ((vehicleData.m_flags & Vehicle.Flags.Stopped) != 0 && !flag && CanLeave(__instance, vehicleID, ref vehicleData))
+		    {
+                if(newTarget != 0)
                 {
-                    vehicleData.Info.m_vehicleAI.SetTarget(vehicleID, ref vehicleData, newTarget);
-
-                    //// Print debug data to log
-                    //BuildingManager instance = Singleton<BuildingManager>.instance;
-                    //float tempDistance = VectorUtils.LengthXZ(vehicleData.GetLastFramePosition() - instance.m_buildings.m_buffer[newTarget].m_position);
-                    //UnityEngine.Debug.Log("Firetruck ID: " + vehicleID + " target set " + newTarget + " distance " + tempDistance);
-
-                    // If the fire truck is stopped, the new target building is close enough that it will not move again so retarget deployed firefighting cims
-                    if ((vehicleData.m_flags & Vehicle.Flags.Stopped) != 0)
-                    {
-                        NewFireAI.TargetCimsParentVehicleTarget(vehicleID, ref vehicleData);
-                    }
-
+                    NewFireAI.TargetCimsParentVehicleTarget(vehicleID, ref vehicleData);
                 }
-            }
+                else
+                {
+                    vehicleData.m_flags &= ~Vehicle.Flags.Stopped;
+			        vehicleData.m_flags |= Vehicle.Flags.Leaving;
+                }
+		    }
+		    if ((vehicleData.m_flags & Vehicle.Flags.GoingBack) == 0)
+		    {
+			    if (ShouldReturnToSource(__instance, vehicleID, ref vehicleData))
+			    {
+				    SetTarget(__instance, vehicleID, ref vehicleData, 0);
+			    }
+		    }
+		    else if (((Singleton<SimulationManager>.instance.m_currentFrameIndex >> 4) & 0xF) == (vehicleID & 0xF) && !ShouldReturnToSource(__instance, vehicleID, ref vehicleData))
+		    {
+			    TransferManager.TransferOffer offer = default(TransferManager.TransferOffer);
+			    offer.Priority = 3;
+			    offer.Vehicle = vehicleID;
+			    offer.Position = frameData.m_position;
+			    offer.Amount = 1;
+			    offer.Active = true;
+			    Singleton<TransferManager>.instance.AddIncomingOffer((TransferManager.TransferReason)vehicleData.m_transferType, offer);
+		    }
+            return false;
         }
     }
 
     // Firecopters respond to fires that they fly near when waiting for a target or going back to the depot
-    [HarmonyPatch(typeof(FireCopterAI), "SimulationStep", new Type[] { typeof(ushort), typeof(Vehicle), typeof(Vehicle.Frame), typeof(ushort), typeof(Vehicle), typeof(int) }, new ArgumentType[] { ArgumentType.Normal, ArgumentType.Ref, ArgumentType.Ref, ArgumentType.Normal, ArgumentType.Ref, ArgumentType.Normal })]
+    [HarmonyPatch(typeof(FireCopterAI))]
     public static class FireCopterAISimulationStepPatch
     {
-        public static void Postfix(ushort vehicleID, ref Vehicle vehicleData)
-        {
-            if ((vehicleData.m_flags & Vehicle.Flags.GoingBack) != 0)
-            {
-                ushort newTarget = NewFireAI.FindBurningBuilding(vehicleData.GetLastFramePosition(), 500f);
+        private delegate void FindFillLocationDelegate(FireCopterAI instance, ushort vehicleID, ref Vehicle data);
+        private static FindFillLocationDelegate FindFillLocation = AccessTools.MethodDelegate<FindFillLocationDelegate>(typeof(FireTruckAI).GetMethod("FindFillLocation", BindingFlags.Instance | BindingFlags.NonPublic), null, false);
+
+        private delegate void HelicopterAISimulationStepDelegate(HelicopterAI instance, ushort vehicleID, ref Vehicle vehicleData, ref Vehicle.Frame frameData, ushort leaderID, ref Vehicle leaderData, int lodPhysics);
+        private static readonly HelicopterAISimulationStepDelegate BaseSimulationStep = AccessTools.MethodDelegate<HelicopterAISimulationStepDelegate>(typeof(HelicopterAI).GetMethod("SimulationStep", BindingFlags.Instance | BindingFlags.Public, null, new Type[] { typeof(ushort), typeof(Vehicle).MakeByRefType(), typeof(Vehicle.Frame).MakeByRefType(), typeof(ushort), typeof(Vehicle).MakeByRefType(), typeof(int) }, new ParameterModifier[] { }), null, false);
+        
+        private delegate uint FindBurningTreeDelegate(FireCopterAI instance, int seed, Vector3 pos, float maxDistance, Vector3 priorityPos);
+        private static FindBurningTreeDelegate FindBurningTree = AccessTools.MethodDelegate<FindBurningTreeDelegate>(typeof(FireTruckAI).GetMethod("FindBurningTree", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static), null, false);
+
+        private delegate bool ExtinguishFire1Delegate(FireCopterAI instance, ushort vehicleID, ref Vehicle data, ushort buildingID, ref Building buildingData);
+        private static ExtinguishFire1Delegate ExtinguishFire1 = AccessTools.MethodDelegate<ExtinguishFire1Delegate>(typeof(FireTruckAI).GetMethod("ExtinguishFire", BindingFlags.Instance | BindingFlags.NonPublic, null, new Type[] { typeof(ushort), typeof(Vehicle).MakeByRefType(), typeof(ushort), typeof(Building).MakeByRefType() }, new ParameterModifier[] { }), null, false);
+
+        private delegate bool ExtinguishFire2Delegate(FireCopterAI instance, ushort vehicleID, ref Vehicle data, uint treeID, ref TreeInstance treeData);
+        private static ExtinguishFire2Delegate ExtinguishFire2 = AccessTools.MethodDelegate<ExtinguishFire2Delegate>(typeof(FireTruckAI).GetMethod("ExtinguishFire", BindingFlags.Instance | BindingFlags.NonPublic, null, new Type[] { typeof(ushort), typeof(Vehicle).MakeByRefType(), typeof(uint), typeof(TreeInstance).MakeByRefType() }, new ParameterModifier[] { }), null, false);
+
+        private delegate void SetTargetDelegate(FireCopterAI instance, ushort vehicleID, ref Vehicle data, ushort targetBuilding);
+        private static SetTargetDelegate SetTarget = AccessTools.MethodDelegate<SetTargetDelegate>(typeof(FireTruckAI).GetMethod("SetTarget", BindingFlags.Instance | BindingFlags.Public), null, false);
+
+        private delegate bool ShouldReturnToSourceDelegate(FireCopterAI instance, ushort vehicleID, ref Vehicle data);
+        private static ShouldReturnToSourceDelegate ShouldReturnToSource = AccessTools.MethodDelegate<ShouldReturnToSourceDelegate>(typeof(FireTruckAI).GetMethod("ShouldReturnToSource", BindingFlags.Instance | BindingFlags.NonPublic), null, false);
+
+        [HarmonyPatch(typeof(FireCopterAI), "SimulationStep", 
+        new Type[] { typeof(ushort), typeof(Vehicle), typeof(Vehicle.Frame), typeof(ushort), typeof(Vehicle), typeof(int) }, 
+        new ArgumentType[] { ArgumentType.Normal, ArgumentType.Ref, ArgumentType.Ref, ArgumentType.Normal, ArgumentType.Ref, ArgumentType.Normal })]
+
+        public static bool SimulationStep(FireCopterAI __instance, ushort vehicleID, ref Vehicle vehicleData, ref Vehicle.Frame frameData, ushort leaderID, ref Vehicle leaderData, int lodPhysics)
+	    {
+		    frameData.m_blinkState = (((leaderData.m_flags & (Vehicle.Flags.Emergency1 | Vehicle.Flags.Emergency2)) == 0) ? 0f : 10f);
+		    if (leaderData.m_transferSize == 0 && (leaderData.m_flags & Vehicle.Flags.GoingBack) == 0 && (leaderData.m_flags & (Vehicle.Flags.Emergency1 | Vehicle.Flags.Emergency2)) != 0)
+		    {
+			    FindFillLocation(__instance, leaderID, ref leaderData);
+		    }
+		    BaseSimulationStep(__instance, vehicleID, ref leaderData, ref frameData, leaderID, ref leaderData, lodPhysics);
+		    bool flag = false;
+		    if (leaderData.m_targetBuilding != 0)
+		    {
+			    if (leaderData.m_transferType == 68)
+			    {
+				    BuildingManager instance = Singleton<BuildingManager>.instance;
+				    BuildingInfo info = instance.m_buildings.m_buffer[leaderData.m_targetBuilding].Info;
+				    Vector3 position = instance.m_buildings.m_buffer[leaderData.m_targetBuilding].m_position;
+				    float maxDistance = info.m_buildingAI.MaxFireDetectDistance(leaderData.m_targetBuilding, ref instance.m_buildings.m_buffer[leaderData.m_targetBuilding]);
+				    uint num = FindBurningTree(__instance, vehicleID, position, maxDistance, frameData.m_position);
+				    if (num != 0)
+				    {
+					    TreeManager instance2 = Singleton<TreeManager>.instance;
+					    position = Singleton<TreeManager>.instance.m_trees.m_buffer[num].Position;
+					    if (VectorUtils.LengthSqrXZ(position - frameData.m_position) < 100f)
+					    {
+						    if ((leaderData.m_flags & Vehicle.Flags.Emergency2) != 0)
+						    {
+							    leaderData.m_flags = (leaderData.m_flags & ~Vehicle.Flags.Emergency2) | Vehicle.Flags.Emergency1;
+						    }
+						    if (ExtinguishFire2(__instance, leaderID, ref leaderData, num, ref instance2.m_trees.m_buffer[num]) && FindBurningTree(__instance, vehicleID, position, maxDistance, frameData.m_position) == 0)
+						    {
+							    SetTarget(__instance, leaderID, ref leaderData, 0);
+						    }
+					    }
+					    else if ((leaderData.m_flags & Vehicle.Flags.Emergency1) != 0 && VectorUtils.LengthSqrXZ(position - frameData.m_position) >= 900f)
+					    {
+						    leaderData.m_flags = (leaderData.m_flags & ~Vehicle.Flags.Emergency1) | Vehicle.Flags.Emergency2;
+					    }
+				    }
+				    else
+				    {
+					    SetTarget(__instance, leaderID, ref leaderData, 0);
+				    }
+			    }
+			    else
+			    {
+				    BuildingManager instance3 = Singleton<BuildingManager>.instance;
+				    instance3.m_buildings.m_buffer[leaderData.m_targetBuilding].CalculateMeshPosition(out var meshPosition, out var _);
+				    if (VectorUtils.LengthSqrXZ(meshPosition - frameData.m_position) < 64f)
+				    {
+					    if ((leaderData.m_flags & Vehicle.Flags.Emergency2) != 0)
+					    {
+						    leaderData.m_flags = (leaderData.m_flags & ~Vehicle.Flags.Emergency2) | Vehicle.Flags.Emergency1;
+					    }
+					    if (ExtinguishFire1(__instance, leaderID, ref leaderData, leaderData.m_targetBuilding, ref instance3.m_buildings.m_buffer[leaderData.m_targetBuilding]))
+					    {
+						    SetTarget(__instance, leaderID, ref leaderData, 0);
+					    }
+				    }
+				    else
+				    {
+					    if ((leaderData.m_flags & Vehicle.Flags.Emergency1) != 0)
+					    {
+						    leaderData.m_flags = (leaderData.m_flags & ~Vehicle.Flags.Emergency1) | Vehicle.Flags.Emergency2;
+					    }
+					    if (instance3.m_buildings.m_buffer[leaderData.m_targetBuilding].m_fireIntensity == 0)
+					    {
+						    SetTarget(__instance, leaderID, ref leaderData, 0);
+					    }
+				    }
+			    }
+		    }
+            if ((leaderData.m_flags & Vehicle.Flags.GoingBack) != 0)
+		    {
+			    ushort newTarget = NewFireAI.FindBurningBuilding(vehicleData.GetLastFramePosition(), 500f);
                 if (newTarget != 0)
                 {
-                    //// Debug info to solve despawn issue
-                    //UnityEngine.Debug.Log(vehicleData.m_flags);
-                    //UnityEngine.Debug.Log("Transfer type is: " + vehicleData.m_transferType);
-
                     // Switch to building extinguishing material if carrying tree extinguishing material
                     // If this is not done, copter will despawn if targeted at a burning building
-                    if(vehicleData.m_transferType == 68)
+                    if(leaderData.m_transferType == 68)
                     {
-                        vehicleData.m_transferType = 71;
+                        leaderData.m_transferType = 71;
                     }
-
-                    vehicleData.Info.m_vehicleAI.SetTarget(vehicleID, ref vehicleData, newTarget);
-
-                    //// Debug info to solve despawn issue
-                    //UnityEngine.Debug.Log(vehicleData.m_flags);
-                    //// Print debug data to log
-                    //BuildingManager instance = Singleton<BuildingManager>.instance;
-                    //float tempDistance = VectorUtils.LengthXZ(vehicleData.GetLastFramePosition() - instance.m_buildings.m_buffer[newTarget].m_position);
-                    //UnityEngine.Debug.Log("FireCopter ID: " + vehicleID + " target set " + newTarget + " distance " + tempDistance);
+                    SetTarget(__instance, leaderID, ref leaderData, newTarget);
                 }
-            }
-        }
+		    }
+		    if ((leaderData.m_flags & Vehicle.Flags.GoingBack) == 0)
+		    {
+			    if (ShouldReturnToSource(__instance, leaderID, ref leaderData))
+			    {
+				    SetTarget(__instance, leaderID, ref leaderData, 0);
+			    }
+			    else if (leaderData.m_transferSize == 0 && (leaderData.m_flags & (Vehicle.Flags.Emergency1 | Vehicle.Flags.Emergency2)) != 0)
+			    {
+				    FindFillLocation(__instance, leaderID, ref leaderData);
+			    }
+		    }
+		    else if (((Singleton<SimulationManager>.instance.m_currentFrameIndex >> 4) & 0xF) == (leaderID & 0xF) && !ShouldReturnToSource(__instance, leaderID, ref leaderData))
+		    {
+			    TransferManager.TransferOffer offer = default(TransferManager.TransferOffer);
+			    offer.Priority = 3;
+			    offer.Vehicle = leaderID;
+			    offer.Position = frameData.m_position;
+			    offer.Amount = 1;
+			    offer.Active = true;
+			    Singleton<TransferManager>.instance.AddIncomingOffer((TransferManager.TransferReason)leaderData.m_transferType, offer);
+		    }
+            return false;
+	    }
     }
 }
